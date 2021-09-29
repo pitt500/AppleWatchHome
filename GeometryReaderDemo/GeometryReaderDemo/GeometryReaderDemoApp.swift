779e135975e0d7ad062fdb0c6b5245628a7bfde4
pitt500 <>
2021-09-28 19:13:45 -0700

    Add initial code

---
+++ GeometryReaderDemo/GeometryReaderDemo/GeometryReaderDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeometryReaderDemoApp.swift
+//  GeometryReaderDemo
+//
+//  Created by Pedro Rojas on 28/09/21.
+//
+
+import SwiftUI
+
+@main
+struct GeometryReaderDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
