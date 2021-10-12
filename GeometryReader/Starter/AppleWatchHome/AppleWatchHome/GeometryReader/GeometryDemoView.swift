73ef501554ff2679e7d10e7617bb4b4a01ffaf60
pitt500 <>
2021-10-11 17:52:04 -0700

    Add Geometry Reader folder

---
+++ GeometryReader/Starter/AppleWatchHome/AppleWatchHome/GeometryReader/GeometryDemoView.swift
@@ -0,0 +1,20 @@
+//
+//  GeometryDemoView.swift
+//  AppleWatchHome
+//
+//  Created by Pedro Rojas on 11/10/21.
+//
+
+import SwiftUI
+
+struct GeometryDemoView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct GeometryDemoView_Previews: PreviewProvider {
+    static var previews: some View {
+        GeometryDemoView()
+    }
+}
