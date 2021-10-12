0a6c47d106d46330a11cc880302aeac89aecdf97
pitt500 <>
2021-10-12 06:43:56 -0700

    Add scaling and maths to get the distance from edge to center

--- GeometryReader/Final/AppleWatchHome/AppleWatchHome/Apple Watch/AppleWatchView.swift
+++ GeometryReader/Final/AppleWatchHome/AppleWatchHome/Apple Watch/AppleWatchView.swift
@@ -26,28 +26,37 @@ struct AppleWatchView: View {
         ZStack {
             Color.black
                 .edgesIgnoringSafeArea([.all])
-
             ScrollView([.horizontal, .vertical], showsIndicators: false) {
                 LazyVGrid(
                     columns: gridItems,
                     alignment: .center,
                     spacing: Self.spacingBetweenRows
                 ) {
-
                     ForEach(0..<1000) { value in
-
-                        Image(appName(value))
-                            .resizable()
-                            .cornerRadius(Self.size/2)
-                            .frame(height: Self.size)
-                            .offset(
-                                x: offsetX(value),
-                                y: 0
-                            )
+                        GeometryReader { proxy in
+                            Image(appName(value))
+                                .resizable()
+                                .cornerRadius(Self.size/2)
+                                .frame(height: Self.size)
+                                .scaleEffect(
+                                    scale(
+                                        proxy: proxy,
+                                        value: value
+                                    )
+                                )
+                                .offset(
+                                    x: offsetX(value),
+                                    y: 0
+                                )
+                        }
+                        // You need to add height
+                        .frame(
+                            width: Self.size,
+                            height: Self.size
+                        )
                     }
                 }
             }
-
         }
     }
 
@@ -63,6 +72,71 @@ struct AppleWatchView: View {
 
     func appName(_ value: Int) -> String {
         apps[value%apps.count]
+    }
+
+    func scale(proxy: GeometryProxy, value: Int) -> CGFloat {
+        let rowNumber = value / gridItems.count
+        let appIndex = value%apps.count
+
+        let x = (rowNumber % 2 == 0) ? proxy.frame(in: .global).midX + proxy.size.width/2 :
+        proxy.frame(in: .global).midX
+
+        let y = proxy.frame(in: .global).midY
+        let center: CGPoint = CGPoint(
+            x: UIScreen.main.bounds.size.width*0.5,
+            y: UIScreen.main.bounds.size.height*0.5
+        )
+
+        let maxDistanceToCenter = getDistanceFromEdgeToCenter(x: x, y: y)
+
+        let point = CGPoint(x: x, y: y)
+        let result = distanceBetweenPoints(p1: center, p2: point)
+
+
+
+        let total = min(abs(result - maxDistanceToCenter), maxDistanceToCenter*0.7)
+
+        let finalResult = total/(maxDistanceToCenter) * 1.4
+
+        return finalResult
+    }
+
+    //distance2
+    func getDistanceFromEdgeToCenter(x: CGFloat, y: CGFloat) -> CGFloat {
+        // This needs to be updated every time we rotate.
+        let center: CGPoint = CGPoint(
+            x: UIScreen.main.bounds.size.width*0.5,
+            y: UIScreen.main.bounds.size.height*0.5
+        )
+
+        let m = (center.y - y)/(center.x - x)
+        let angle = abs(atan(m) * 180 / .pi)
+
+        let ipadAngle: CGFloat = 35
+
+        if angle > ipadAngle {
+            let yEdge = (y > center.y) ? center.y*2 : 0
+            let xEdge = (yEdge - y)/m + x
+            let edgePoint = CGPoint(x: xEdge, y: yEdge)
+
+            return distanceBetweenPoints(p1: center, p2: edgePoint)
+        } else {
+            let xEdge = (x > center.x) ? center.x*2 : 0
+            let yEdge = m * (xEdge - x) + y
+            let edgePoint = CGPoint(x: xEdge, y: yEdge)
+            return distanceBetweenPoints(p1: center, p2: edgePoint)
+        }
+    }
+
+    func distanceBetweenPoints(p1: CGPoint, p2: CGPoint) -> CGFloat {
+        let xDistance = abs(p2.x - p1.x)
+        let yDistance = abs(p2.y - p1.y)
+
+        return CGFloat(
+            sqrt(
+                pow(xDistance, 2) + pow(yDistance, 2)
+            )
+        )
     }
 }
 
