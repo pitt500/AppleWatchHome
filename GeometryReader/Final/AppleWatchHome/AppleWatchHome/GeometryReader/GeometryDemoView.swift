3e2274340c911aeded4f7a711f35efa62f239070
pitt500 <>
2021-10-13 07:40:25 -0700

    Add GeometryReader demo

--- GeometryReader/Final/AppleWatchHome/AppleWatchHome/GeometryReader/GeometryDemoView.swift
+++ GeometryReader/Final/AppleWatchHome/AppleWatchHome/GeometryReader/GeometryDemoView.swift
@@ -9,7 +9,37 @@ import SwiftUI
 
 struct GeometryDemoView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Color.red
+            Color.green
+                .padding(40)
+            GeometryReader { proxy in
+                VStack(alignment: .leading) {
+                    Color.blue
+                        .padding(proxy.size.width*0.1)
+                        .frame(
+                            width: proxy.size.width/2,
+                            height: proxy.size.height/2
+                        )
+                    Text("Local min x: \(proxy.frame(in: .local).minX)")
+                    Text("Local min y: \(proxy.frame(in: .local).minY)")
+                    Text("Local max x: \(proxy.frame(in: .local).maxX)")
+                    Text("Local max y: \(proxy.frame(in: .local).maxY)")
+                    Text("size: \(proxy.size.width)")
+
+                    Text("Global min x: \(proxy.frame(in: .global).minX)")
+                    Text("Global min y: \(proxy.frame(in: .global).minY)")
+                    Text("Global max x: \(proxy.frame(in: .global).maxX)")
+                    Text("Global max y: \(proxy.frame(in: .global).maxY)")
+
+                }
+                .font(.system(size: 24))
+
+            }
+            .background(.gray)
+            .offset(x: 50, y: 50)
+            .frame(width: 300, height: 300)
+        }
     }
 }
 
