9ef68c0e0b93b15a93175918a616c6500d86b59f
pitt500 <>
2021-10-14 06:47:14 -0700

    Add Apple Watch view as starting point

--- GeometryReader/Final/AppleWatchHome/AppleWatchHome/AppleWatchHomeApp.swift
+++ GeometryReader/Final/AppleWatchHome/AppleWatchHome/AppleWatchHomeApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct AppleWatchHomeApp: App {
     var body: some Scene {
         WindowGroup {
-            GeometryDemoView()
+            AppleWatchView()
         }
     }
 }
